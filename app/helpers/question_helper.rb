606b8e91930e5ee94f5abe0d4b61971132f878f3
Екатерина Иониди <ekaterinaionidi@MacBook-Pro-Ekaterina.local>
2017-01-19 21:27:30 +0300

    fix

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,5 +1,3 @@
 module QuestionHelper
-  @questions_count = @questions.count
-  @answers_count = @questions.where.not(answer: nil).count
-  @unanswered_count = @questions_count - @answers_count
+  
 end
