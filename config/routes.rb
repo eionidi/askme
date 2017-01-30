d10ad82b5d47958be35e66c5ef899e162104cb50
Екатерина Иониди <ekaterinaionidi@MacBook-Pro-Ekaterina.local>
2017-01-31 00:38:14 +0300

    security

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,8 @@ Rails.application.routes.draw do
   resources :users
 
   # ресурс сессий (только три экшена :new, :create, :destroy)
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   # ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
-
-
-  #  синонимы путей - в дополнение к созданным в ресурсах выше
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
