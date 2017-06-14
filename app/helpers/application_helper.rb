ab2fb2860e997a0796812f547979ea35c73ad26d
Catherine <eionidi@artec-group.com>
2017-06-14 21:54:42 +0300

    locales

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,28 +23,32 @@ module ApplicationHelper
   def count_questions
     I18n.t("question", count: @questions.count)
   end
-  
+
+  def count_answers
+    I18n.t("answer", count: @answers.count)
+  end
+
   # def sklonenie(number, krokodil, krokodila, krokodilov)
 #     # проверим входные данные на правильность
 #     if (number == nil || !number.is_a?(Numeric))
 #       number = 0 # если первый параметр пустой или не число, то продолжаем как будто он нулевой
 #     end
-# 
+#
 #     ostatok = number % 10 # склонение определяется последней цифрой в числе
-# 
+#
 #     ostatok100 = number % 100
 #     if (ostatok100 >= 11 && ostatok100 <= 14)
 #       return krokodilov
 #     end
-# 
+#
 #     if (ostatok == 1) # для 1 - именительный падеж (Кто? Что?)
 #       return krokodil
 #     end
-# 
+#
 #     if (ostatok >= 2 && ostatok <= 4) # для 2-4 - родительный падеж (Кого? Чего?)
 #       return krokodila
 #     end
-# 
+#
 #     # 5-9 или ноль – родительный падеж и множественное число
 #     if (ostatok >= 5 && ostatok <= 9 || ostatok == 0)
 #       return krokodilov
